00bea27586d79209d7c959763e5bb9c231bfb07a
Raslif.A <raslif1234@gmail.com>
2022-08-06 13:06:30 +0400

    Add react-router for routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
+import PageNotFound from "./components/Error/PageNotFound";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      {/* Header Component */}
-      <Header />
-
-      {/* Home Component */}
-      <Home />
-
-      {/* Footer Component */}
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Error/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Error.css";
+
+function PageNotFound() {
+  return (
+    <div className="page__notfound__error">
+      The page you are looking for is not available
+    </div>
+  );
+}
+
+export default PageNotFound;
