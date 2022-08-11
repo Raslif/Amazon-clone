f4698f3b8475a8c552d3ef92595ec4001b68c3ea
Raslif.A <raslif1234@gmail.com>
2022-08-11 10:21:21 +0400

    Add the sign-in and register user functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../firebase";
+
 import "./Login.css";
 
 function Login() {
+  let navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+    // add proper validation rules before the authentication
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  const onRegsiter = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        if (user) {
+          debugger;
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
     <div className="login">
-      <div className="login__logo">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" />
-      </div>
+      <Link to="/">
+        <div className="login__logo">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" />
+        </div>
+      </Link>
 
       <div className="login__form">
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" value="" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" value="" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button type="submit" className="login__signin__button">
+          <button
+            type="submit"
+            className="login__signin__button"
+            onClick={onSubmit}
+          >
             Sign In
           </button>
         </form>
@@ -26,7 +79,7 @@ function Login() {
           By signing-in you agree to the AMAZON CLONE Conditions of Use & Sale.
         </p>
 
-        <button className="login__register__button">
+        <button className="login__register__button" onClick={onRegsiter}>
           Create your Amazon Account
         </button>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,20 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDSzT4dhrQkQrmbazzTrtktzBOmA9-FmAc",
-    authDomain: "clone-149b5.firebaseapp.com",
-    projectId: "clone-149b5",
-    storageBucket: "clone-149b5.appspot.com",
-    messagingSenderId: "1007361563529",
-    appId: "1:1007361563529:web:fffe32673c4ef03ce51f58",
-    measurementId: "G-KFXRDZLGH9"
+  apiKey: "AIzaSyDSzT4dhrQkQrmbazzTrtktzBOmA9-FmAc",
+  authDomain: "clone-149b5.firebaseapp.com",
+  projectId: "clone-149b5",
+  storageBucket: "clone-149b5.appspot.com",
+  messagingSenderId: "1007361563529",
+  appId: "1:1007361563529:web:fffe32673c4ef03ce51f58",
+  measurementId: "G-KFXRDZLGH9",
 };
+
+const firebaseInitializer = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseInitializer.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
